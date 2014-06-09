a34c959fd09ed72e1efeb07dbfd31240f9706488
ksoichiro <soichiro.kashima@gmail.com>
2014-06-10 00:23:22 +0900

    Added WhitespaceAfterOrOperator rule for Objective-C.

--- fint_test.go
+++ fint_test.go
@@ -31,7 +31,7 @@ const (
 	LocaleDefault             = "en"
 	LocaleJa                  = "ja"
 	TemplateDefault           = "default"
-	ErrorsObjcNormal          = 61
+	ErrorsObjcNormal          = 62
 )
 
 func TestExecuteAsCommand(t *testing.T) {
